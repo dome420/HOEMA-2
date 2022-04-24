5484d0dab93754be7d488a639c526c901fd3bdac
dome420 <72267274+dome420@users.noreply.github.com>
2022-04-24 16:58:08 +0200

    Aufgabe 01 Fertig

--- Implementierung Gradientenverfahren/ClassVektor.h
+++ Implementierung Gradientenverfahren/ClassVektor.h
@@ -5,24 +5,36 @@
     {
     private:
         /* data */
-        int Dimension = 0, Value = 0, key = 0;
         std::vector<double>Vector;
     public:
+        // Constructor & Destructor
         CMyVektor();
         ~CMyVektor();
 
-        //Public Method
-        void set_Dimension(int Dimension /* Aktuelle Dim*/);
-        void set_specified_Value(int index, int Value);
-        double get_specified_Value(int key);
-        int get_length_Vector();
+        //setter
+        void set_Dimension(int Dimension /* Aktuelle Dim*/); // Set Dimension of a Vector
+        void set_specified_Value(int index, int Value); // overwrites a new value at a specific index
+        //getter
+        int get_dimension() { return Vector.size();}; // return vector size("dim")
+        double get_specified_Value(int key); // return value at a specific index
+        int get_dim(); // return Vector size ("dim")
+        double get_length(CMyVektor& x); // return length of Vector with sqrt(pow(x[i],2)
+        //überladungen
         double& operator [](int index);
+        friend std::ostream& operator<<(std::ostream& os, CMyVektor x);
+     
     };
 
 
-    CMyVektor::CMyVektor(/* args */){}
-
-    CMyVektor::~CMyVektor(){}
+    CMyVektor::CMyVektor()
+    {
+        Vector.assign(1, 0.0);
+    }
+  
+    CMyVektor::~CMyVektor()
+    {
+        Vector.clear();
+    }
 
 // (2) Dimension bestimmen können
 void CMyVektor::set_Dimension(int Dimension /* Aktuelle Dim*/)
@@ -43,7 +55,16 @@ double CMyVektor::get_specified_Value(int key)
         }
     }
 };
-int CMyVektor::get_length_Vector()
+double get_length(CMyVektor& x)
+{
+    double length = 0;
+    for (unsigned i = 0; i < x.get_dim(); i++)
+    {
+       length += pow(x[i], 2);
+    }
+    return sqrt(length);
+}
+int CMyVektor::get_dim()
 {
     return Vector.size();
 };
@@ -51,4 +72,45 @@ int CMyVektor::get_length_Vector()
 double& CMyVektor::operator [](int index)
 {
     return Vector[index];
+}
+std::ostream& operator<<(std::ostream& os, CMyVektor x)
+{
+    os << "(";
+    for (size_t i = 0; i < x.get_dimension(); i++) {
+        os << x[i];
+        if (i != x.Vector.size() - 1) {
+            os << ";";
+        }
+    }
+    os << ")";
+    return os;
+}
+CMyVektor operator+(CMyVektor a, CMyVektor b)
+{
+    int ai = 0, bi = 0;
+    int counter = 0;
+    CMyVektor c;
+    c.set_Dimension(a.get_dim());
+    // The dimension of the vector should be the same
+    if (a.get_dim() == b.get_dim())
+    {
+        while (counter < a.get_dim())
+        {
+            c[counter] = a[ai] + b[bi];
+            counter++;
+            ai++;
+            bi++;
+        }
+        return c;
+    }
+}
+CMyVektor operator*(double lambda, CMyVektor a)
+{
+    CMyVektor c;
+    c.set_Dimension(a.get_dim());
+    for (unsigned i = 0; i < a.get_dim(); i++)
+    {
+        c[i] += lambda * a[i];
+    }
+    return c;
 }
--- Implementierung Gradientenverfahren/main.cpp
+++ Implementierung Gradientenverfahren/main.cpp
@@ -3,11 +3,9 @@
 
 using namespace std;
 
-CMyVektor operator+(CMyVektor a, CMyVektor b);
-CMyVektor operator*(double lambda, CMyVektor a);
 
 int main() {
-
+	// Addition und Multiplikation
 	CMyVektor V1;
 	CMyVektor V2;
 	V1.set_Dimension(2);
@@ -16,78 +14,14 @@ int main() {
 	V2.set_Dimension(2);
 	V2.set_specified_Value(0, 1);
 	V2.set_specified_Value(1, 1);
+	CMyVektor V3;
+	V3.set_Dimension(V1.get_dim());
+	int lambda = 2;
+	V3 = V1 + V2;
+	CMyVektor lam = lambda * V2;
+	cout << V3 << endl << lam;
 
-	//int V_Size = 0,int Specified_Value_key = 0, double Value = 0,double Vector_Value = 0;
-	double lamda = 2;
-	char input;
-
-	/*while (input != 'q')
-	{
-		do {
-			cout << "Geben Sie die Vektor Groeße des ersten Vektors ein : ";
-			cin >> V_Size;
-
-		} while (V_Size <= 0);
-
-		V1.set_Dimension(V_Size);
-		V_Size = 0;
-		// (1) Werte eingabe für den ersten Vektor
-		do {
-			cout << "Geben Sie die Vektor Groeße des ersten Vektors ein : ";
-			cin >> V_Size;
-
-		} while (V_Size <= 0);
-		V1.set_Dimension(V_Size);
-		// (1) Werte eingabe für den zweiten Vektor
-		V_Size = 0;
-
-		cout << endl;
-		cout << "Eine bestimmte Komponente des Vektors setzen (key) : ";
-	}*/
-	
-
-	CMyVektor C;
-	C.set_Dimension(V1.get_length_Vector());
-	C= V1 + V2;
-	std::cout << "Addition : "<< "(";;
-	for (int i = 0; i < C.get_length_Vector(); i++)
-	{
-		
-			std::cout  << C[i] << " ";
-		
-		
-	}
-	std::cout << ")" << endl;
-	C = lamda * C;
-	std::cout << "Skalarprodukt: "<< C[0]<< " ";
-
+	// gradient
 }
 
-CMyVektor operator+(CMyVektor a, CMyVektor b)
-{
-	int ai = 0, bi = 0;
-	int counter = 0;
-	CMyVektor c;
-	c.set_Dimension(a.get_length_Vector());
-	// Wenn Dimension Gleich dann addition
-	if (a.get_length_Vector() == b.get_length_Vector())
-	{
-		while (counter < a.get_length_Vector())
-		{
-			c[counter] = a[ai] + b[bi];
-			counter++;
-		}
-		return c;
-	}
 
-}
-CMyVektor operator*(double lambda, CMyVektor a) 
-{
-	CMyVektor c;
-	c.set_Dimension(1);
-	for (unsigned i = 0; i < a.get_length_Vector(); i++)
-	{
-		c[0] += lambda * a[i];
-	}
-	return c;
-}
